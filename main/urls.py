9c0bc9a42bfcad55d2deb3c3019e0f00d3b7f4cd
Agil89 <agil.makhmudov@mail.ru>
2021-05-19 16:19:45 +0400

    new verss

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns=[
     path('main/', HomePage.as_view(), name='home'),
-    path('login/',CustomLoginView.as_view(),name='login'),
+    path('',CustomLoginView.as_view(),name='login'),
     path('create/',CreateCsvView.as_view(),name='create'),
     path('download/',DownloadView.as_view(),name='download'),
     path('logout/', LogoutView.as_view(), name='logout'),
